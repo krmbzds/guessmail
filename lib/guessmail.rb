df0b1cc2624aff5d6ff2bf63777f562be5826a7e
Kerem Bozdas <krmbzds.github@gmail.com>
2016-01-28 00:10:37 +0200

    Initial commit

---
+++ lib/guessmail.rb
@@ -0,0 +1,5 @@
+require "guessmail/version"
+
+module Guessmail
+  # Your code goes here...
+end
---
+++ spec/guessmail_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Guessmail do
+  it 'has a version number' do
+    expect(Guessmail::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
